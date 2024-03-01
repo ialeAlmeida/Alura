ba8c4e5903343129e14546e63cc261ddd2e189ef
iale.almeida_ <almeidaialesantos@gmail.com>
2024-03-01 11:44:22 -0300

    Add new folder: OO - Módurlo 04

---
+++ Exercícios/OO - Módulo 04/src/exercicioAula/Episodio.java
@@ -0,0 +1,9 @@
+public class Episodio implements Classificavel {
+    @Override
+    int getClassificacao() {
+        return 9;
+    }
+    if{
+        da
+    }
+}
---
+++ Exercícios/OO - Módulo 04/src/exercicioAula/Filme.java
@@ -0,0 +1,6 @@
+public class Filme implements Classificavel {
+    @Override 
+    int getClassificacao() {
+        return 8;
+    }
+}
