43d8b71ccf7c4c3a612d04b9d9c34ee53381f1af
Iale Almeida <143238811+ialeAlmeida@users.noreply.github.com>
2024-03-07 00:11:09 -0300

    FIXING ERROR - Update Musica.java

--- Java/Projetos/OO/src/Musica.java
+++ Java/Projetos/OO/src/Musica.java
@@ -1,21 +1,12 @@
-import java.util.ArrayList;
-
 public class Musica extends Audio {
     private int idMusica;
-    private String artista;
-    private ArrayList<String> artistas = new ArrayList<String>();
+    private String cantor;
     
-    //sobrecarga de construtor
-    public Musica(int idMusica, String nome, String artista, double tempoDeReproducao, boolean disponivelNoSpotify, boolean disponivelNoYoutube) {
+    //construtor
+    public Musica(int idMusica, String nome, String cantor, double tempoDeReproducao, boolean disponivelNoSpotify, boolean disponivelNoYoutube) {
         super(nome, tempoDeReproducao, disponivelNoSpotify, disponivelNoYoutube);
         this.idMusica = idMusica;
-        this.artista = artista;
-    }
-
-    public Musica(int idMusica, String nome, double tempoDeReproducao, boolean disponivelNoSpotify, boolean disponivelNoYoutube) {
-        super(nome, tempoDeReproducao, disponivelNoSpotify, disponivelNoYoutube);
-        this.idMusica = idMusica;
-        System.out.println("Para adicionar os artista dessa música, execute o método: registraArtista()");
+        this.cantor = cantor;
     }
 
     //getters
@@ -23,8 +14,8 @@ public class Musica extends Audio {
         return idMusica;
     }
 
-    public String getArtista() {
-        return artista;
+    public String getcantor() {
+        return cantor;
     }
 
     @Override 
@@ -32,21 +23,17 @@ public class Musica extends Audio {
         System.out.println(String.format("""
                    --- Ficha Técnica ---
                 Nome:       %s
-                Artista:    %s
-                Tempo de Reprodução:    %.2f
+                cantor:    %s
+                Tempo de reprodução:    %.2f
+                Total de reproduções:   %d
                 Disponível no Spotify:  %s
                 Disponível no Youtube:  %s
-                """, nome, artista, tempoDeReproducao, disponivelNoSpotify, disponivelNoYoutube));
+                """, nome, cantor, tempoDeReproducao, totalReproducoes, disponivelNoSpotify, disponivelNoYoutube));
     }
 
-    public void registraArtista() {
-        System.out.println("Quantos artistas fizeram parte dessa música?");
-        int qntdArtista = s.nextInt();
-        
-        for (int i = 0; i < qntdArtista; i++) {
-            System.out.println("Digite o nome de um dos artistas(um a um, até acabar): ");
-            String artista = s.nextLine();
-            artistas.add(artista);
-        }
+    @Override
+    public String reproduzAudio() {
+        totalReproducoes++;
+        return "Música foi reproduzido";
     }
 }
