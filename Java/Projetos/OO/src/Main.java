6fe28bc0ca7d92e840d8d7853029a4a89cd36538
iale.almeida_ <almeidaialesantos@gmail.com>
2024-03-06 23:46:52 -0300

    Update in all -> Added reproduzAudio()

---
+++ Java/Projetos/OO/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Audio a = new Audio("Não sou demais", 2.48, true, true);
+        // a.mostraFichaTecnica();
+        // a.curteAudio();
+         a.comentaAudio();
+        // a.mostraComentarios();
+        // a.avaliaAudio();
+        // a.mostraAvaliacoes();
+         a.menu();*/
+
+        Musica m = new Musica(01, "Carnificina", 3.40, true, true);
+        m.registraArtista();
+    }
+
+}
