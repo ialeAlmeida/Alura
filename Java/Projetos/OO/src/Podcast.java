b06e58dc156aa7a0eed69a97690fb58cce025fc3
Iale Almeida <143238811+ialeAlmeida@users.noreply.github.com>
2024-03-07 00:11:46 -0300

    FIXING ERROR - Update Podcast.java

--- Java/Projetos/OO/src/Podcast.java
+++ Java/Projetos/OO/src/Podcast.java
@@ -1,37 +1,49 @@
-// import java.util.ArrayList;
-
-// public class Podcast extends Audio {
-//     private int idPodcast;
-//     private String apresentador;
-//     private ArrayList<String> apresentadores = new ArrayList<String>();
-//     private String convidado;
-//     private ArrayList<String> convidados = new ArrayList<String>();
-
+public class Podcast extends Audio {
+    private int idPodcast;
+    private String apresentador;
+    private String convidado;
     
-//     //sobrecarga de construtor
-//     public Musica(String nome, double tempoDeReproducao, boolean disponivelNoSpotify, boolean disponivelNoYoutube, int idMusica, String artista) {
-//         super(nome, tempoDeReproducao, disponivelNoSpotify, disponivelNoYoutube);
-//         this.idMusica = idMusica;
-//         this.artista = artista;
-//     }
+    //sobrecarga de construtor
+    public Podcast(int idPodcast, String nome, String apresentador, String convidado, double tempoDeReproducao, boolean disponivelNoSpotify, boolean disponivelNoYoutube) {
+        super(nome, tempoDeReproducao, disponivelNoSpotify, disponivelNoYoutube);
+        this.idPodcast = idPodcast;
+        this.apresentador = apresentador;
+        this.convidado = convidado;
+    }
 
-//     public int getIdMusica() {
-//         return idMusica;
-//     }
+    //getters e setter
+    public int getIdPodcast() {
+        return idPodcast;
+    }
 
-//     public String getArtista() {
-//         return artista;
-//     }
+    public String getApresentador() {
+        return apresentador;
+    }
 
-//     @Override 
-//     public void mostraFichaTecnica() {
-//         System.out.println(String.format("""
-//                    --- Ficha Técnica ---
-//                 Nome:       %s
-//                 Artista:    %s
-//                 Tempo de Reprodução:    %.2f
-//                 Disponível no Spotify:  %s
-//                 Disponível no Youtube:  %s
-//                 """, nome, artista, tempoDeReproducao, disponivelNoSpotify, disponivelNoYoutube));
-//     }
-// }
+    public String getConvidado() {
+        return convidado;
+    }
+    public void setConvidado(String convidado) {
+        this.convidado = convidado;
+    }
+
+    @Override 
+    public void mostraFichaTecnica() {
+        System.out.println(String.format("""
+                   --- Ficha Técnica ---
+                Nome:            %s
+                Apresentador:    %s
+                Convidado:       %s
+                Tempo de reprodução:    %.2f
+                Total de reproduções:   %d
+                Disponível no Spotify:  %s
+                Disponível no Youtube:  %s
+                """, nome, apresentador, convidado, tempoDeReproducao,totalReproducoes, disponivelNoSpotify, disponivelNoYoutube));
+    }
+
+    @Override
+    public String reproduzAudio() {
+        totalReproducoes++;
+        return "Podcast foi reproduzido";
+    }
+}
